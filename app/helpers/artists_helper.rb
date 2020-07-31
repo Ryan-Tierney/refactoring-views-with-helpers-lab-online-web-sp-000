6817bfb739223b93be80190afc1d28e85202aca1
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-31 15:40:17 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
   
   def display_artist
     if song.artist
-      link_to artist_path(@artist)
+      link_to song.artist.name artist_path(@artist)
     else 
       link_to song_path(@song)
     end 
